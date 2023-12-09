003c88c7ec57ff7abdb055d3928e75d3a24470cc
Yucheng Low <2740522+ylow@users.noreply.github.com>
2023-12-08 21:10:11 -0800

    day9

---
+++ day9/9a.py
@@ -0,0 +1,17 @@
+def extra(v):
+    assert(len(v) > 0)
+    if all([x == v[0] for x in v]):
+        return v[0]
+    t=[]
+    for i in range(len(v) - 1):
+        t.append(v[i+1] - v[i])
+    return v[-1] + extra(t)
+
+f = open("input", 'r')
+ext = []
+for line in f:
+    vals = [int(x) for x in line.split(' ')]
+    ext.append(extra(vals))
+
+print(ext)
+print(sum(ext))
